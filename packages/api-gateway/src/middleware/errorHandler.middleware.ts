d41014f74f20c34c33b1759aeb2637c7dee61065
minacryptoo <manu_p12@icloud.com>
2026-09-09 17:20:04 +0200

    Actualización desde Carnets

---
+++ packages/api-gateway/src/middleware/errorHandler.middleware.ts
@@ -0,0 +1,6 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+export async function errorHandler(error: any, request: FastifyRequest, reply: FastifyReply) {
+  console.error(error);
+  reply.status(500).send({ error: 'Internal Server Error', message: error.message });
+}

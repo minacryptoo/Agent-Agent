9cc632d6d1b086c4f9f54eeb6e92e44d5810f4d4
minacryptoo <manu_p12@icloud.com>
2026-09-09 17:02:01 +0200

    Actualización desde Carnets

---
+++ packages/frontend/next.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['ipfs.io', 'arweave.net'],
+  },
+};
+module.exports = nextConfig;

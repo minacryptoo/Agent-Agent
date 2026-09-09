50cb17e58c4fb4853452fa2fdfc5f015d978ed63
minacryptoo <manu_p12@icloud.com>
2026-09-09 17:02:09 +0200

    Actualización desde Carnets

---
+++ packages/frontend/lib/utils.ts
@@ -0,0 +1,18 @@
+
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatAddress(address: string) {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
+
+export function formatUSDC(amount: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+}
